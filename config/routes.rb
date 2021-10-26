7fb590cafdf6a91f1027c19de0c8468f697de2c7
migestrada <miguelp.estradam@gmail.com>
2021-10-25 22:56:41 -0300

    Config devise jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'web_client#index'
 
   namespace :api do
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ bundle exec rake assets:precompile
 
 # Clean a stale pid file
 rm -f tmp/pids/server.pid
+rails db:drop
+rails db:create
+rails db:migrate
 
 # Run the main container process, inside the Bundler context
 exec bundle exec "$@"
